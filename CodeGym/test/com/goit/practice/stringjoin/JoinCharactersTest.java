5a19b80f02101113c08492523f4faae067927438
i.pylyavets <ihor.pylyavets@gmail.com>
2016-04-21 19:43:37 +0300

    first commit, add JoinCharacters

---
+++ CodeGym/test/com/goit/practice/stringjoin/JoinCharactersTest.java
@@ -0,0 +1,38 @@
+package com.goit.practice.stringjoin;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JoinCharactersTest {
+    JoinCharacters joinCharacters = new JoinCharacters();
+
+    @Test
+    public void testSingleElementArray() throws Exception {
+        char[] input = {'1'};
+        int expected = 1;
+
+        int actualResult = joinCharacters.join(input);
+
+        Assert.assertEquals("Characters should be joined correctly for single element array", expected, actualResult);
+    }
+
+    @Test
+    public void testSingleElementArrayWithZero() throws Exception {
+        char[] input = {'0'};
+        int expected = 0;
+
+        int actualResult = joinCharacters.join(input);
+
+        Assert.assertEquals("Characters should be joined correctly for single element array with zero", expected, actualResult);
+    }
+
+    @Test
+    public void testThreeElementArray() throws Exception {
+        char[] input = {'1', '8', '3'};
+        int expected = 183;
+
+        int actualResult = joinCharacters.join(input);
+
+        Assert.assertEquals("Characters should be joined correctly for three element array", expected, actualResult);
+    }
+}
