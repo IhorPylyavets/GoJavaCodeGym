07a1c3e78dacf285d325d979c985e25d3e9eba0b
i.pylyavets <ihor.pylyavets@gmail.com>
2016-04-27 11:59:18 +0300

    add PositiveAverageNumber

---
+++ CodeGym/src/com/goit/practice/stringjoin/PositiveAverageNumber.java
@@ -0,0 +1,10 @@
+package com.goit.practice.stringjoin;
+
+public class PositiveAverageNumber {
+    public int average(int a, int b) {
+        if (a == b) {
+            return a;
+        }
+        return (a + b)/2;
+    }
+}
---
+++ CodeGym/test/com/goit/practice/stringjoin/PositiveAverageNumberTest.java
@@ -0,0 +1,47 @@
+package com.goit.practice.stringjoin;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vbevz on 4/27/2016.
+ */
+public class PositiveAverageNumberTest {
+    PositiveAverageNumber positiveAverageNumber = new PositiveAverageNumber();
+
+    @Test
+    public void testAverage() throws Exception {
+        int a = 2;
+        int b = 4;
+        //average( 4, 7 ) = 5
+        int expected = 3;
+
+        int actualResult = positiveAverageNumber.average(a, b);
+
+        Assert.assertEquals("PositiveAverageNumber average be count correctly for 2,4", expected, actualResult);
+    }
+
+    @Test
+    public void testAverageV2() throws Exception {
+        int a = 4;
+        int b = 7;
+        int expected = 5;
+
+        int actualResult = positiveAverageNumber.average(a, b);
+
+        Assert.assertEquals("PositiveAverageNumber average be count correctly for 4,7", expected, actualResult);
+    }
+
+    @Test
+    public void testAverageV3() throws Exception {
+        int a = 1073741824;
+        int b = 1073741824;
+        int expected = 1073741824;
+
+        int actualResult = positiveAverageNumber.average(a, b);
+
+        Assert.assertEquals("PositiveAverageNumber average be count correctly for 1073741824,1073741824", expected, actualResult);
+    }
+}
