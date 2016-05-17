eb94ab3c213357e99a066acfb3bcd936c7e53d68
i.pylyavets <ihor.pylyavets@gmail.com>
2016-05-17 08:06:52 +0300

    fix joinCharacters && sumDigits

--- CodeGym/src/com/goit/practice/stringjoin/JoinCharacters.java
+++ CodeGym/src/com/goit/practice/stringjoin/JoinCharacters.java
@@ -10,7 +10,7 @@ public class JoinCharacters {
     public int join(char[] input) {
         int result = 0;
         for (int i = 0; i < input.length; i++) {
-            result = result*10 + (input[i]-'0');
+            result = result * 10 + (input[i] - '0');
         }
         return result;
     }
--- CodeGym/src/com/goit/practice/stringjoin/SumDigits.java
+++ CodeGym/src/com/goit/practice/stringjoin/SumDigits.java
@@ -6,23 +6,11 @@ package com.goit.practice.stringjoin;
 * */
 public class SumDigits {
     public int sum(int number) {
-        return sumOfDigitUsingRecursion(number, 0);
-    }
-
-    private int sumOfDigitUsingRecursion(int number, int sum) {
-        if (number == 0) {
-            return sum;
+        int result = 0;
+        while (number != 0) {
+            result += number % 10;
+            number /= 10;
         }
-
-        int recursionNumber = number / 10;
-        int addResult = number % 10;
-
-        if (addResult < 0) {
-            addResult *= -1;
-            recursionNumber *= -1;
-        }
-
-        sum += addResult;
-        return sumOfDigitUsingRecursion(recursionNumber, sum);
+        return Math.abs(result);
     }
 }
